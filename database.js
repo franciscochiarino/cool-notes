6c7b3048367468c5f2bf47de34819f27d6be7694
Fran <francisco.chiarino@gmail.com>
2023-07-15 22:09:07 +0200

    change notes

--- database.js
+++ database.js
@@ -14,19 +14,19 @@ export const notes = [
   {
     id: "d0ca5b636d0398",
     title: 'Questions',
-    description: "Why do we park in driveways and drive on parkways?\nIf two mind readers read each other's minds, whose mind are they actually reading?",
+    description: "If two mind readers read each other's minds, whose mind are they actually reading?",
     pinned: 'true'
   },
   {
     id: "d09188a97c3608",
     title: 'Thoughts',
-    description: 'People have different opinions on how to live life, but everyone is just trying to figure it out.',
+    description: 'People have different opinions on how to live life, but everyone is living for the first time.',
     pinned: 'false'
   },
   {
     id: "69a21370ca3128",
     title: 'Southern Sayings',
-    description: "He's got his nose so high in the air, he could drown in a rainstorm.",
+    description: "He's got his nose so high up in the sky, he could drown in a rainstorm.",
     pinned: 'false'
   }
 ];
